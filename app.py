f81416bca79ddc73cffbb5ec5539abddcd748210
NilanjanAdak <niladak94@gmail.com>
2025-01-29 17:21:42 +0530

    .env variables added

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "proj-travel-plan"
-app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///F:\sqlite-tools-win-x64-3480000\travellan.db"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///" + os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
