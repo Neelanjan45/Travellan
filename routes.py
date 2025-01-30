c17d15ad3600fab00a2ebb1284816ec9db4b18fd
NilanjanAdak <niladak94@gmail.com>
2025-01-30 18:40:22 +0530

    Sites view added

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from models import Locations, Sites
+from sqlalchemy import inspect
 
 @app.route("/")
 @app.route("/home")
@@ -11,3 +12,8 @@ def home():
 def locations():
     locations = Locations.query.all()
     return render_template('locations.html', locations=locations)
+
+@app.route("/sites")
+def sites():
+    sites = Sites.query.all()
+    return render_template('sites.html', sites=sites)
