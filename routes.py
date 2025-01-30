dc75dc51311e70b33ea1077af39742c48582bd2d
NilanjanAdak <niladak94@gmail.com>
2025-01-30 20:20:07 +0530

    Specific location view added

--- routes.py
+++ routes.py
@@ -11,3 +11,9 @@ def home():
 def locations():
     locations = Locations.query.all()
     return render_template('locations.html', locations=locations)
+
+@app.route("/location/<int:loc_id>")
+def location(loc_id):
+    location = Locations.query.get(loc_id)
+    sites = Sites.query.filter_by(loc_id=loc_id).all()
+    return render_template('location.html', location=location, sites=sites)
