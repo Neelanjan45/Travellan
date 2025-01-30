7a995a39912ce664c188c49fe4d1eceda0dc7713
NilanjanAdak <niladak94@gmail.com>
2025-01-30 16:58:33 +0530

    Locations view created

--- routes.py
+++ routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
+from models import Locations, Sites
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('index.html')
+
+@app.route("/locations")
+def locations():
+    locations = Locations.query.all()
+    return render_template('locations.html', locations=locations)
